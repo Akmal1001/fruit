c3f2a52b14bf455028f9a5670eff07099008eee2
Akmal <akmalagzamov95@gmail.com>
2023-09-12 21:08:57 +0500

    english words

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,75 @@
-import 'package:dartcode/view/home_page.dart';
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
 
-void main() => runApp(NavApp());
-
-class NavApp extends StatelessWidget {
-  const NavApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: FirstScreen());
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  List<String> myList = [
+    "Book",
+    'Note',
+    'Pen',
+    'making',
+    'was',
+    'her',
+    'not',
+    'She',
+    'about',
+    'time.',
+    'on',
+    'appointment',
+    'anxious',
+    "Book",
+    'Note',
+    'Pen',
+    'making',
+    'was',
+    'her',
+    'not',
+    'She',
+    'about',
+    'time.',
+    'on',
+    'appointment',
+    'anxious',
+    "Book",
+    'Note',
+    'Pen',
+    'making',
+    'was',
+    'her',
+    'not',
+    'She',
+    'about',
+    'time.',
+    'on',
+    'appointment',
+    'anxious'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lesson 16"),
+      ),
+      body: Container(
+        child: ListView(
+          children: myList.map((e) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(child: Text(e)),
+            );
+          }).toList(),
+        ),
+      ),
+    );
   }
 }
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,57 +1,14 @@
-import 'package:dartcode/view/second_screen.dart';
-import 'package:dartcode/view/third_screen.dart';
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/src/widgets/container.dart';
+// import 'package:flutter/src/widgets/framework.dart';
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({Key? key}) : super(key: key);
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    Widget screenPreferences(
-        {required String screenName,
-        required Widget buttons,
-        required Widget buttons1}) {
-      return Scaffold(
-        backgroundColor: Color(0xFF262020),
-        appBar: AppBar(
-          backgroundColor: Colors.green[700],
-          title: Text(screenName),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [buttons, buttons1],
-            ),
-          ),
-        ),
-      );
-    }
-
-    Widget buttonBuilder(
-        {required Widget screenWidget, required String buttonName}) {
-      return ElevatedButton(
-        style: ElevatedButton.styleFrom(primary: Colors.green[700]),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => screenWidget,
-            ),
-          );
-        },
-        child: Text(buttonName),
-      );
-    }
-
-    return screenPreferences(
-        screenName: 'First Screen',
-        buttons: buttonBuilder(
-            screenWidget: SecondScreen(), buttonName: 'Go to Second Screen'),
-        buttons1: buttonBuilder(
-            screenWidget: ThirdScreen(), buttonName: 'Go to Third Screen'));
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('App'),),
+//     );
+//   }
+// }
